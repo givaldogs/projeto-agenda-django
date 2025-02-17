224b7a905779c2ff0a9c9b39d3a0fc95db533936
givaldogs <givaldowanda@gmail.com>
2025-02-17 12:54:31 -0300

    Adicionando mais campo no formulario

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,28 @@
-from django.shortcuts import get_object_or_404,  render, redirect
-from django.http import Http404
-#from django.http import HttpResponse
-from contact.models import Contact
-from django.db.models import Q
-from django.core.paginator import Paginator
+#from typing import Any, Dict
+#from django.http import Http404
+#from django.db.models import Q
+#from django.core.paginator import Paginator
+#from contact.models import Contact
+#
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 # Create your views here.
 
 def create(request):
-    
-    context = {}
+    if request.method == 'POST':
+        context = {
+        'form': ContactForm(data=request.POST)
+        }
+        return render(
+               request,
+               'contact/create.html',
+                context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
 
     return render(
         request,
