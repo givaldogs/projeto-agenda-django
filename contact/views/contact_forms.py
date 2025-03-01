c4317de87867553111fb077b66a8a36293fa38c3
givaldogs <givaldowanda@gmail.com>
2025-03-01 17:16:19 -0300

    ocultando links de update e delete de nao owner dos contatos

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -7,9 +7,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     if request.method == 'POST':
@@ -20,7 +22,8 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
             contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
@@ -41,9 +44,12 @@ def create(request):
         context
     )
 
+
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id,
-                                 show=True
+                                 show=True,
+                                 owner = request.user
                                 )
     form_action = reverse('contact:update', args=(contact_id,))
     if request.method == 'POST':
@@ -75,9 +81,12 @@ def update(request, contact_id):
         context
     )
 
+
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id,
-                                 show=True
+                                 show=True,
+                                 owner = request.user
                                 )
     confirmation = request.POST.get('confirmation', 'no')
     if confirmation == 'yes':
